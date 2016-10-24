d1d8d86f378f85408e0202ea6d0d1ec3fafefa6c
Vedikk <maystruk94@gmail.com>
2016-10-25 00:13:48 +0300

    4

--- Practice6/js/script.js
+++ Practice6/js/script.js
@@ -3,10 +3,17 @@
 function fadeIn (){
     document.getElementById('search').style.display='inline-block';
     document.getElementById('fa-search').style.display='none';
-    document.getElementById('search').style.animationName = 'display_on';
 }
 function fadeOut (){
     document.getElementById('search').style.display='none';
-    document.getElementById('search').style.animationName = 'display_off';
     document.getElementById('fa-search').style.display='inline-block';
 }
+function rightIn(){
+    document.getElementById('drop_menu').style.display='block';
+    document.getElementById('bars').style.display='none';
+}
+function rightOut(){
+    document.getElementById('drop_menu').style.display='none';
+    document.getElementById('bars').style.display='inline-block';
+}
+
