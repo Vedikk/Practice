146bfaf0bc7a751dd6e3e9c77999f724e60ac0ce
Vedikk <maystruk94@gmail.com>
2017-01-19 13:22:04 +0200

    added 'gallery', fixed header

--- furniture/js/script.js
+++ furniture/js/script.js
@@ -1,10 +1,46 @@
-/**
- * Created by mayst on 17.01.2017.
- */
+//govnoCod
+
 $(document).ready(function () {
+    //anchor
     $('a[href^="#"]').click(function(){
         var target = $(this).attr('href');
         $('html, body').animate({scrollTop: $(target).offset().top}, 900);
         return false;
     });
+
+    //nav
+    function addHeaderH() {
+        var headerHeight = $('.header').height();
+        $('.top-slider').css('marginTop', headerHeight);
+    }
+    addHeaderH();
+
+
+
+    $(window).resize(addHeaderH);
+
+    function barsClick() {
+        var $nav = $('.nav');
+        $nav.slideToggle('slow');
+
+    }
+    $('#menuBars').click(barsClick);
+
+    //Gallery
+    $(".small-img").click(function(){
+        var img = $(this);
+        var src = img.attr('src');
+        $("body").append("<div class='popup'>"+
+            "<div class='popup_bg'></div>"+
+            "<img src='"+src+"' class='popup_img' />"+
+            "</div>");
+        $(".popup").fadeIn(800);
+        $(".popup_bg").click(function(){
+            $(".popup").fadeOut(800);
+            setTimeout(function() {
+                $(".popup").remove();
+            }, 800);
+        });
+    });
+
 });
